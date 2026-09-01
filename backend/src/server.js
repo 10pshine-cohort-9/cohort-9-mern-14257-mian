e67a7708b1c14746baa398568c9bc8c31b05ed87
Mian Muhammad Jahanzaib <jahanzaibm120@gmail.com>
2026-09-01 17:52:25 +0500

    chore: trigger sonarqube analysis

--- backend/src/server.js
+++ backend/src/server.js
@@ -52,7 +52,7 @@ app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 app.use(pinoHttp({ logger }));
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
